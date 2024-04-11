d62da8fcadb8893b62dd2395dd2f333ec5f804d7
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2024-04-11 07:11:12 +0530

    multer,cloudinary setup

--- random.js
+++
@@ -1,5 +0,0 @@
-import { generateRandomString } from "random-string-generator-library";
-
-// Generate a random string of length 10
-const randomString = generateRandomString(100);
-console.log(randomString);
